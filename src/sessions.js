7b355d182c024a9b6458913b410a104396c590c6
Ahmed Sayed <ahmedsayed@gmail.com>
2026-06-30 22:29:10 +0300

    fix: set standard Windows Chrome user-agent for Puppeteer

--- src/sessions.js
+++ src/sessions.js
@@ -141,6 +141,7 @@ const setupSession = async (sessionId) => {
     }
 
     const clientOptions = {
+      userAgent: 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/126.0.0.0 Safari/537.36',
       puppeteer: {
         executablePath: chromeBin,
         headless,
